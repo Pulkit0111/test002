edb5691fb06b789274d33aba4fd0e1e63b4538b6
Pulkit Tyagi <pulkittyagi@192.168.0.196>
2022-09-19 23:08:53 +0530

    albin fixed code

--- prime.js
+++ prime.js
@@ -1,11 +1,21 @@
-function prime(num){
+function checkPrime(num){
+	let fact=0;
 	for (let i=1;i<=num;i++){
 		if(num%i){
-			return true;
+			fact++;
 		} 
 	}
+	if(fact==0){
+		return true;
+	} 
 	return false;
 }
 
 
-let x=prime(15);
+let check=checkPrime(15);
+
+if(check==true){
+	console.log("prime");
+} else {
+	console.log(prime);
+}
